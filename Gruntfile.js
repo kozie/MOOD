605c13a775eed1c2fabc4e11a6fef373e7f01c02
U-INVENTEERS\vanegmond <vanegmond@PC05.inventeers.local>
2015-01-27 10:13:48 +0100

    Added Grunt stuff to minifi and serve over http. Aww yissh

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,13 +8,31 @@ module.exports = function(grunt) {
         src: ['src/main.ts'],
         dest: 'dist/mood.js'
       }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'dist/mood.min.js': ['<%= typescript.base.dest %>']
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          hostname: '*',
+          keepalive: true
+        }
+      }
     }
   });
 
   // Load plugins
   grunt.loadNpmTasks('grunt-typescript');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   // Register tasks
-  grunt.registerTask('default', ['typescript']);
-
+  grunt.registerTask('default', ['typescript', 'uglify']);
+  grunt.registerTask('go', ['default', 'connect']);
 };
